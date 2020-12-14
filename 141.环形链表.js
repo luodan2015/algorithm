f3cf2729322aa54a71e5c59545c7a3bcd81f7399
luodan <1058470680@qq.com>
2020-12-14 18:37:48 +0800

    feat: 环形链表

---
+++ 141.环形链表.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+// 时间复杂度O(n)空间复杂度O(n)
+var hasCycle1 = function(head) {
+  // 也可以使用对象Map
+  let cache = new Set();
+  while(head) {
+    if (cache.has(head)) {
+      return true;
+    }
+    cache.add(head);
+    head = head.next;
+  }
+  return false;
+};
+// 空间复杂度O(1)
+var hasCycle = function(head) {
+  let slow = head;
+  let fast = head;
+  while(fast?.next) {
+    slow = slow.next;
+    fast = fast.next?.next;
+    if (fast === slow) {
+      return true;
+    }
+  }
+  return false;
+};
+// @lc code=end
+
